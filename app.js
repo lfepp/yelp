ddd4c24a1e480c6d52b559e3bc08ca8fa4ae54d0
lfepp <lucasfepp@gmail.com>
2015-09-07 22:04:42 -0400

    Stop after finished with IDs

--- app.js
+++ app.js
@@ -19,19 +19,35 @@ var temp = -1;
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
   console.log(ids.length);
-  setInterval(function() {
+  var queryInt = setInterval(function() {
     if(temp < i) {
       temp++;
       console.log('Running for index: ' + i);
       yelp.business(ids[i], function(error, data) {
           if(!error) {
-            json[i] = {idSent: ids[i], id: data.id, name: data.name, address: data.location.address[0], city: data.location.city, state: data.location.state_code, zip: data.location.postal_code, country: data.location.country_code, numReviews: data.review_count};
+            json[i] = {
+              idSent: ids[i],
+              id: data.id,
+              url: data.url,
+              isClaimed: data.is_claimed,
+              isClosed: data.is_closed,
+              name: data.name,
+              phone: data.display_phone,
+              address: data.location.address[0],
+              city: data.location.city,
+              state: data.location.state_code,
+              zip: data.location.postal_code,
+              country: data.location.country_code,
+              numReviews: data.review_count};
             console.log('Successfully added:');
             console.dir(json[i]);
             i++;
           } else {
             console.log(error);
             i++;
+            if (i >= ids.length) {
+              clearInterval(queryInt);
+            }
           }
       })
     }
